5cd8ccf2f8e9ab238bbfee63038a8ffdd1693fc2
Alyaa Ali <eissaalyaa@gmail.com>
2023-11-03 19:04:18 +0200

    ...

--- Project/Phase1 - Code F23/Phase1 - Code F22/GUI/Output.cpp
+++ Project/Phase1 - Code F23/Phase1 - Code F22/GUI/Output.cpp
@@ -82,8 +82,7 @@ void Output::CreateDrawToolBar() const
 	string MenuItemImages[DRAW_ITM_COUNT];
 	MenuItemImages[ITM_RECT] = "images\\MenuItems\\Menu_Rect.jpg";
 	MenuItemImages[ITM_EXIT] = "images\\MenuItems\\Menu_Exit.jpg";
-	ssss
-	//MenuItemImages[ITM_CIRC] = "D:\\Year one\\Programming techniques\\Project\\Phase1 - Code F23\\png-transparent-rectangle-shape-rectangle-purple-blue-angle.jpg";
+	MenuItemImages[ITM_CIRC] = "D:\\Year one\\Programming techniques\\Project\\Phase1 - Code F23\\png-transparent-rectangle-shape-rectangle-purple-blue-angle.jpg";
 	
 	//TODO: Prepare images for each menu item and add it to the list
 
