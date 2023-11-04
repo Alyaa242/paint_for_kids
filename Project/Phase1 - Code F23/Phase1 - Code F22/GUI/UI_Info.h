61a07256069edef9b91aea737f9a33a71c9addac
engmohamed-emad <149832636+engmohamed-emad@users.noreply.github.com>
2023-11-04 06:18:01 +0200

    KK

--- Project/Phase1 - Code F23/Phase1 - Code F22/GUI/UI_Info.h
+++ Project/Phase1 - Code F23/Phase1 - Code F22/GUI/UI_Info.h
@@ -25,8 +25,8 @@ enum DrawMenuItem //The items of the Draw menu (you should add more items)
 
 	ITM_EXIT,	//Exit item
 	
-	DRAW_ITM_COUNT		//no. of menu items ==> This should be the last line in this enum
-	
+	DRAW_ITM_COUNT	,	//no. of menu items ==> This should be the last line in this enum
+	ITM_SWITCH  /*++++++++*/
 };
 
 enum PlayMenuItem //The items of the Play menu (you should add more items)
@@ -39,9 +39,6 @@ enum PlayMenuItem //The items of the Play menu (you should add more items)
 	PLAY_ITM_COUNT		//no. of menu items ==> This should be the last line in this enum
 	
 };
-
-
-
 
 
 __declspec(selectany) //This line to prevent "redefinition error"
